d7dce308d6c6ef389edab3b5cd4acbe9dc84b292
ELI-THE_CREATOR <neverlookbackeli@gmail.com>
2025-05-24 19:16:36 +0100

    Initial

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  console.log("App component rendered");
+  return (
+    <div className="w-full h-screen flex flex-col px-5 space-y-8 items-center justify-center ">
+      Test
+    </div>
+  );
+};
+
+export default App;
